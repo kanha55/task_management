924a3f5b6ff29e084e5a770904dd680bd04f9f13
kanha55 <kanhu.dubey9@gmail.com>
2024-06-08 00:32:21 +0530

    add frontend url in fcors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000' # replace with your origins
+    origins 'localhost:3000', '127.0.0.1:3000', 'https://task-management-1-8k99.onrender.com' # replace with your origins
 
     resource '*',
       headers: :any,
